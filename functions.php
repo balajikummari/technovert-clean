fcb3b657df70498797c351cf33be9c3f985f5384
Balaji Kummari <balajievlo@gmail.com>
2021-02-09 18:18:56 +0530

    removed blocklibrary styles

--- functions.php
+++ functions.php
@@ -185,3 +185,14 @@ function tvstyles_enqueue_styles() {
 add_action( 'wp_enqueue_scripts', 'tvstyles_enqueue_styles' );
 
 
+
+
+add_filter('use_block_editor_for_post_type', '__return_false', 10);
+// Don't load Gutenberg-related stylesheets.
+add_action( 'wp_enqueue_scripts', 'remove_block_css', 100 );
+function remove_block_css() {
+wp_dequeue_style( 'wp-block-library' ); // WordPress core
+wp_dequeue_style( 'wp-block-library-theme' ); // WordPress core
+wp_dequeue_style( 'wc-block-style' ); // WooCommerce
+wp_dequeue_style( 'storefront-gutenberg-blocks' ); // Storefront theme
+}
