24ae9714f0c99e7d608c267ee9580754334d1e1d
Amol Borkar <amolborkar5@gmail.com>
2021-02-15 14:46:52 +0530

    Fixed error in technovert.js

--- src/js/technovert.js
+++ src/js/technovert.js
@@ -3,9 +3,9 @@
 
   $(document).ready(function () {
     // Mark current page in sub navigation menu
-    $("ul.subnav li").each(function (i) {
-      if ($(this).find("a").attr("href") === window.locationi.href) {
-        $(this).find("a").addClass("active");
+    $("ul.subnav li a").each(function (i) {
+      if ($(this).attr("href") === window.location.href) {
+        $(this).addClass("active");
       }
     });
   });
