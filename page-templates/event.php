7714d3b7cb5ca17dd245af061092722e40ea4048
Rajesh Swarna <rajeshswarna123@gmail.com>
2021-03-24 13:57:28 +0530

    Events static HTML

--- page-templates/event.php
+++ page-templates/event.php
@@ -28,14 +28,33 @@ $GLOBALS['wp_query'] = new WP_Query( $args );
 $blogquery = $GLOBALS['wp_query']
 
 ?>
+<section class="case-studies">
+  <div class="min-h-100 bg-gray-dark"></div>
+    <div class="container-box">
+      <h4 class="text-gray">Events</h4>
 
+      <div class="filters d-flex mt-20">
+        <select name="industry" id="industry">
+          <option value="0">Industry</option>
+          <option value="1">Banking</option>
+          <option value="2">Insurance</option>
+          <option value="3">RPA</option>
+        </select>
+        <select name="solution" id="solution">
+          <option value="0">Solution</option>
+        </select>
+        <input type="text" id="search" placeholder="Search" onkeyup="searchFilter('.case-study-card', 'h2');" />
+      </div>
 
-<section>
-    <div class="card-circular">
-		<img src="https://staging3.technovert.com/wp-content/uploads/Artificial-Intelligence.svg">
+      
+    <div class="card-circular case-study-card">
+		<!-- <img src="https://staging3.technovert.com/wp-content/uploads/Artificial-Intelligence.svg"> -->
 		<h5 class="">Cloud Transformation</h5>
-		<p class="">From an Idea into a product,accelerate your product development</p>
-		<a class="learn-more" href="#">Learn More</a>
+		<h5>Example heading <span class="badge badge-secondary">WEBINAR</span></h5>
+        <span>Jan 24 Wednesday, 2021</span>
+		<a class="learn-more" href="#">More info</a>
 	</div>
+  </div>
 </section>
+
 <?php get_footer(); ?>
