44530f54fe1e199bca40b631cb35ba6efb06525d
Amol Borkar <amolborkar5@gmail.com>
2021-03-24 12:30:01 +0530

    Fixed favicon not working

--- header.php
+++ header.php
@@ -46,7 +46,7 @@
     <meta name="twitter:title" content="<?php echo(META_TITLE); ?>" />
     <meta name="twitter:description" content="<?php echo(META_DESC); ?>" />
 
-		<link rel="icon" href="/static/images/favicons/favicon.png" sizes="192x192">
+		<link rel="icon" href="/static/images/favicon/favicon.png" sizes="192x192">
 
     <!--Nunito Sans Font Family-->
 		<link
