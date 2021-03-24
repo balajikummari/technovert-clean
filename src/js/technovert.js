f6b6adace37b0d096395644e26d9fa93a9d5a326
Amol Borkar <amolborkar5@gmail.com>
2021-03-24 09:34:38 +0530

    added search filter

--- src/js/technovert.js
+++ src/js/technovert.js
@@ -14,5 +14,38 @@
     if (!isHeroSectionExist) {
       $("header").addClass("coloured-header");
     }
+
+    function searchFilter(itemClass, searchElement) {
+      var query = jQuery("#search").val();
+
+      if (query.length > 0) {
+        jQuery(itemClass).each(function () {
+          if (jQuery(this).find(searchElement).text().indexOf(query) !== -1) {
+            jQuery(this).show();
+          } else {
+            jQuery(this).hide();
+          }
+        });
+      } else {
+        jQuery(itemClass).show();
+      }
+    }
+
+    function locFilter() {
+      var selected = jQuery("select#location :selected").text();
+      if (selected !== "All") {
+        jQuery(".jobRecord").each(function () {
+          var loc = jQuery(this).find("h3 + span").text();
+
+          if (selected === loc) {
+            jQuery(this).show();
+          } else {
+            jQuery(this).hide();
+          }
+        });
+      } else {
+        jQuery(".jobRecord").show();
+      }
+    }
   });
 })(jQuery); // End of use strict
