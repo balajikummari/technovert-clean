ac1288894f62e4adac8bea22a0c4475be95966ce
Rajesh Swarna <rajeshswarna123@gmail.com>
2021-03-25 19:26:35 +0530

    Removed multiple sections in event details page

--- single-event.php
+++ single-event.php
@@ -35,47 +35,42 @@ while ( have_posts() ) : the_post();
 			</p>
 		</div>
 	</div>
-</section>
-<section class="container-box ">
-    <div class="row mx-0">
-        <div class="blurb-img-left mb-50 col-6 p-0">
-            <div class="blurb-image">
-                <img src="/static/images/events/schedule.svg" width="60" alt="event_date_and_time">
-            </div>
-            <div class="blurb-container-content">
-                <span class="mb-10">Date & Time</span>
-                <p class="text-gray">
-                    <?php echo date("M j, Y", strtotime(get_field("event_date"))) ?>
+    <div class="">
+        <div class="row mx-0">
+            <div class="blurb-img-left mb-50 col-6 p-0">
+                <div class="blurb-image">
+                    <img src="/static/images/events/schedule.svg" width="60" alt="event_date_and_time">
+                </div>
+                <div class="blurb-container-content">
+                    <span class="mb-10">Date & Time</span>
+                    <p class="text-gray">
+                        <?php echo date("M j, Y", strtotime(get_field("event_date"))) ?>
 
-                    <a href="#" class="ml-3">Add to calendar</a>
-                </p>
+                        <a href="#" class="ml-3">Add to calendar</a>
+                    </p>
+                </div>
+            </div>
+            <div class="blurb-img-left mb-50 col-6">
+                <div class="blurb-image">
+                    <img src="/static/images/events/location-1.svg" width="60" alt="event_location">
+                </div>
+                <div class="blurb-container-content">
+                    <span class="mb-10">Location</span>
+                    <p class="text-gray">
+                        <?php the_field('event_location')  ?>
+                    </p>
+                </div>
             </div>
         </div>
-        <div class="blurb-img-left mb-50 col-6">
-            <div class="blurb-image">
-                <img src="/static/images/events/location-1.svg" width="60" alt="event_location">
-            </div>
-            <div class="blurb-container-content">
-                <span class="mb-10">Location</span>
-                <p class="text-gray">
-                    <?php the_field('event_location')  ?>
-                </p>
-            </div>
-        </div>
+        <button class="button-right-arrow px-60 rounded">Register</button>
     </div>
-    <button class="button-right-arrow px-60 rounded">Register</button>
-</section>
-<?php the_content(); ?>
-<!-- <section class="container-box mt-80">
-	<div class="feature-content no-underline col-9">
+    <div class="feature-content no-underline col-9">
 		<h2 class="">Who is this for</h2>
 		<p class="">
             NPCA embarked on a digital transformation journey in adapting a cloud-first strategy with a rollout of Office 365, Microsoft Teams and Power Platform. NPCA began looking for modern solutions that could automate and streamline many of their processes and turned to Technovert for help.
 		</p>
 	</div>
-</section>
-<section class="container-box mt-80">
-	<div class="feature-content no-underline col-9">
+    <div class="feature-content no-underline col-9">
 		<h2 class="">What you’ll learn</h2>
 		<p class="">
         NPCA embarked on a digital transformation journey in adapting a cloud-first strategy with a rollout of Office 365, Microsoft Teams and Power Platform.
@@ -95,9 +90,7 @@ while ( have_posts() ) : the_post();
             </div>
         </div>
 	</div>
-</section>
-<section class="container-box mt-80">
-	<div class="no-underline">
+    <div class="no-underline">
 		<h2 class="">Speakers</h2>
 		<p class="">
             We have some distinguished speakers on the event
@@ -117,7 +110,8 @@ while ( have_posts() ) : the_post();
 			</div>
 		</div>
 	</div>
-</section> -->
+</section>
+<!-- <?php the_content(); ?> -->
 
 <?php endwhile; // end of the loop. 
 
