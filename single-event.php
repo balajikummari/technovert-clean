0978a37148aba419b71e1718336a511eb4c5a38d
Rajesh Swarna <rajeshswarna123@gmail.com>
2021-03-25 18:15:21 +0530

    done styling changes to event details page

--- single-event.php
+++ single-event.php
@@ -38,7 +38,7 @@ while ( have_posts() ) : the_post();
 </section>
 <section class="container-box ">
     <div class="row mx-0">
-        <div class="blurb-img-left mb-50 col-6">
+        <div class="blurb-img-left mb-50 col-6 p-0">
             <div class="blurb-image">
                 <img src="/static/images/events/schedule.svg" width="60" alt="event_date_and_time">
             </div>
@@ -63,9 +63,10 @@ while ( have_posts() ) : the_post();
             </div>
         </div>
     </div>
+    <button class="button-right-arrow px-60 rounded">Register</button>
 </section>
 <section class="container-box mt-80">
-	<div class="feature-content no-underline">
+	<div class="feature-content no-underline col-9">
 		<h2 class="">Who is this for</h2>
 		<p class="">
             NPCA embarked on a digital transformation journey in adapting a cloud-first strategy with a rollout of Office 365, Microsoft Teams and Power Platform. NPCA began looking for modern solutions that could automate and streamline many of their processes and turned to Technovert for help.
@@ -73,24 +74,47 @@ while ( have_posts() ) : the_post();
 	</div>
 </section>
 <section class="container-box mt-80">
-	<div class="feature-content no-underline">
+	<div class="feature-content no-underline col-9">
 		<h2 class="">What you’ll learn</h2>
 		<p class="">
         NPCA embarked on a digital transformation journey in adapting a cloud-first strategy with a rollout of Office 365, Microsoft Teams and Power Platform.
 		</p>
-        <ul>
-            <li>The performance management system got set-up in literally 1 week</li>
-            <li>Real-time data and high-quality metrics for better conversations</li>
-            <li>The performance management system got set-up in literally 1 week</li>
-        </ul>
+        <div>
+            <div>
+                <div class="rounded-list-icon mt-3 mx-3"></div>
+                <p class="d-inline-block">The performance management system got set-up in literally 1 week</p>
+            </div>
+            <div>
+                <div class="rounded-list-icon mt-3 mx-3"></div>
+                <p class="d-inline-block">The performance management system got set-up in literally 1 week</p>
+            </div>
+            <div>
+                <div class="rounded-list-icon mt-3 mx-3"></div>
+                <p class="d-inline-block">The performance management system got set-up in literally 1 week</p>
+            </div>
+        </div>
 	</div>
 </section>
 <section class="container-box mt-80">
-	<div class="feature-content no-underline">
+	<div class="feature-content no-underline col-9">
 		<h2 class="">Speakers</h2>
 		<p class="">
             We have some distinguished speakers on the event
 		</p>
+        <div class="col-md-6 col-12 mb-xs-30">
+			<div class="row mt-5">
+				<div class="col-6 text-center">
+					<img class="mb-20" src="/static/images/events/rama_krishna_ch.PNG" alt="Expert 1" width="240">
+					<h5 class="font-weight-extra-bold text-clay">Rama Krishna Ch</h5>
+					<p class="text-sm text-gray">Architect</p>
+				</div>
+				<div class="col-6 text-center">
+					<img class="mb-20" src="/static/images/events/mitra_d.PNG" alt="Expert 2" width="240">
+					<h5 class="font-weight-extra-bold text-clay">BK Saka</h5>
+					<p class="text-sm text-gray">UX Architect</p>
+				</div>
+			</div>
+		</div>
 	</div>
 </section>
 
