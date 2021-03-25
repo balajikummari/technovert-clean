af899d6f593ebfa87b1411fad29c5080e69cc125
Shubham <Ks6422@hotmail.com>
2021-03-25 15:37:54 +0530

    updated few styles in blog

--- single.php
+++ single.php
@@ -11,7 +11,11 @@ get_header();
 ?>
 
 <section class="container-box mt-80">
-		<h1><?php the_title(); ?></h1>
+		<div class="row">
+			<div class="col-md-8 col-12">
+				<h1 class="mb-20"><?php the_title(); ?></h1>
+			</div>
+		</div>
 		<div class="row">
 			<div class="col-md-8 col-12">
 
@@ -50,12 +54,12 @@ get_header();
 			</div>
 			<div class="col-md-4 col-12">
 				<div class="subscribe">
-					<h5 class="mb-10">Join 3500 of your HR peers and stay relevant on latest trends</h5>
+					<h5 class="mb-10 font-weight-extra-bold">Join 3500 of your HR peers and stay relevant on latest trends</h5>
 					<p class="text-sm text-gray-200 mb-20">Don’t miss out on the latest tips, tools, and tactics at the forefront of HR and Employee</p>
 					<form id="email-subscribe">
 						<div class="input-group input-group-lg">
 							<input type="text" class="form-control form-control-sm" placeholder="Enter email address" aria-label="Enter email address" aria-describedby="basic-addon2">
-							<button class="btn btn-primary btn-input btn-input-sm" type="button" id="button-addon2">Subscribe</button>
+							<button class="btn btn-primary btn-input " type="button" id="button-addon2">Subscribe</button>
 						</div>
 					</form>
 				</div>
