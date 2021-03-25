94217562983760374a46493eecb54250b2428d7f
Amol Borkar <amolborkar5@gmail.com>
2021-03-25 11:01:06 +0530

    New template part for category nav

---
+++ template-parts/vertical-nav.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Template part for displaying the vertical nav for category
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Technovert_Clean
+ */
+?>
+
+<div class="p-30 shadow-low rounded">
+  <ul class="vertical-nav">
+    <li class="nav-items">
+      <a href="/category/digital-transformation/" class="nav-link active">Digital Transformation</a>
+    </li>
+    <li class="nav-items">
+      <a href="/category/cloud/" class="nav-link">Cloud</a>
+    </li>
+    <li class="nav-items">
+      <a href="/category/product-engineering/" class="nav-link">Product Engineering</a>
+    </li>
+    <li class="nav-items">
+      <a href="/category/quality-engineering/" class="nav-link">Quality Engineering</a>
+    </li>
+    <li class="nav-items">
+      <a href="/category/user-experience/" class="nav-link">User Experience</a>
+    </li>
+  </ul>
+</div>
