6863236dc4fae989dae56ad2a2c623539c1d3f00
Rajesh Swarna <rajeshswarna123@gmail.com>
2021-03-26 16:37:01 +0530

    Done banner related changes in JD page

--- single-job-detail.php
+++ single-job-detail.php
@@ -24,13 +24,18 @@ while ( have_posts() ) : the_post();
 	</div>
 </section>
 <section class="container-box mt-80">
+	<div class="row">
+    	<div class="col-md-3"></div>
+		<div class="col-md-9 col-sm-12">
+			<div class="feature-content mb-50 no-underline">
+				<h2 class="pb-2"><?php the_title();  ?></h2>
+				<p><?php the_field('job_introduction') ?></p>
+			</div>
+		<div>
+    </div>
 	<div class="row m-0 banner-container">
 		<div class="col-md-9 col-sm-12">
 			<div class="mb-40 mb-xs-80">
-				<div class="feature-content mb-50 no-underline">
-					<h2 class="pb-2"><?php the_title();  ?></h2>
-					<p><?php the_field('job_introduction') ?></p>
-				</div>
 				<div class="feature-content mb-50 no-underline">
 					<h2 class="pb-2">Skills matter some, Attitude matters most</h2>
 					<p>Skills can be learnt and unlearnt any time. But the attitude you bring to the table makes all the difference and gives purpose to those skills. That’s what matters to us most. </p>
