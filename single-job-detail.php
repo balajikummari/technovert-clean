4bb011258ac35fa3ca2268caa9c60c1e2ac25c55
Rajesh Swarna <rajeshswarna123@gmail.com>
2021-03-26 12:51:21 +0530

    Fixed build issue

--- single-job-detail.php
+++ single-job-detail.php
@@ -14,8 +14,8 @@ get_header();
 while ( have_posts() ) : the_post(); 
 ?>
 
-<section class="hero-section" style=";background-position: right 2% bottom 5%;
-    background-size: 35rem, auto;background-image:  url(<?php the_field('feature_image_2'), url(<?php the_field('feature_image') ?>)" >
+<section class="hero-section" style="background-image:  url(<?php the_field('feature_image_2') ?>), url(<?php the_field('feature_image')?>);background-position: right 2% bottom 5%;
+    background-size: 35rem, auto;" >
 	<div class="container-box">
         <span>We are hiring</span>
 		<h1>
