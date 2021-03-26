36540910f065d015ba5d1a97a7fce0569da4c331
Amol Borkar <amolborkar5@gmail.com>
2021-03-26 20:59:28 +0530

    Fixed typo

--- template-parts/insights-nav.php
+++ template-parts/insights-nav.php
@@ -9,29 +9,16 @@
 
 ?>
 
-<!-- <section class="bg-blue">
-    <div class="container-box clear-margin-b">
-        <div class="sub-nav-bar">
-            <p class="d-none d-md-block">INSIGHTS:</p>
-            <ul>
-                <li><a class="active" href="/blog">Articles</a></li>
-                <li><a href="/case-studies">Case studies</a></li>
-                <li><a href="/events">Events</a></li>
-            </ul>
-        </div>
-    </div>
-</section> -->
-
 <p class="sub-navbar-title mobile-visibility d-md-none container-box clear-margin-b">INSIGHTS</p>
 <nav class="navbar navbar-expand-lg sub-navbar">
     <div class="mobile-sub-navbar">
         <p class="sub-navbar-title text-white d-none d-md-block desktop-visibility">INSIGHTS</p>
         <ul class="navbar-nav">
             <li class="nav-item">
-                <a class="nav-link" href="/blog" >Articles</a>
+                <a class="nav-link" href="/blog">Articles</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="/case-studies">studies</a>
+                <a class="nav-link" href="/case-studies">Case Studies</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="/events">Events</a>
