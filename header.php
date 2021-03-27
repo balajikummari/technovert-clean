18130c946b848c902d9aa41751a1ed7c42d63b25
Amol Borkar <amolborkar5@gmail.com>
2021-03-27 17:42:56 +0530

    Fixed about link

--- header.php
+++ header.php
@@ -120,7 +120,7 @@
                             <a class="nav-link" href="/blog">Insights</a>
                         </li>
                         <li class="nav-item dropdown small-dropdown">
-                            <a class="nav-link dropdown-toggle" id="about" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            <a class="nav-link dropdown-toggle" id="about" href="/about" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                 <span>About</span><span class="icon icon-xs text-white ic-chevron-down"></span>
                             </a>
                             <ul class="dropdown-menu" aria-labelledby="about">
