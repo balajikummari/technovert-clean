38d53b0d86deced919b372061378e54f108bc111
Amol Borkar <amolborkar5@gmail.com>
2021-04-01 10:49:06 +0530

    Increased blog posts per page to 12

--- page-templates/technovert-blog.php
+++ page-templates/technovert-blog.php
@@ -14,7 +14,7 @@ get_header();
 
 $page = (get_query_var('paged')) ? get_query_var('paged') : 1;
 $args = array(
-    'posts_per_page'   => 1,
+    'posts_per_page'   => 12,
     'post_status'      => 'publish',
     'order'            => 'DESC',
     'orderby'          => 'date',
