5a9799d1b4a69b89cd487904e9146d56e6035d82
Shubham <Ks6422@hotmail.com>
2021-04-05 17:24:27 +0530

    added social share button template

---
+++ template-parts/social-share-template.php
@@ -0,0 +1,17 @@
+<div class="social-media">
+    <span>Share article</span>
+    <div class="d-flex">
+        <a href="https://www.facebook.com/sharer/sharer.php?u=<?php echo $url; ?>" target="_blank"
+            class="dot dot-sm bg-blue-400 mr-12">
+            <span class="icon icon-xs ic-facebook text-white"></span>
+        </a>
+        <a href="https://twitter.com/intent/tweet?text=<?php echo $title; ?>&amp;url=<?php echo $url; ?>&amp;via=KekaDotCom"
+            target="_blank" class="dot dot-sm bg-blue-300 mr-12">
+            <span class="icon icon-xs ic-twitter text-white"></span>
+        </a>
+        <a href="http://www.linkedin.com/shareArticle?mini=true&url=<?php echo $url; ?>" target="_blank"
+            class="dot dot-sm bg-blue-200 mr-12">
+            <span class="icon icon-xs ic-linkedin text-white"></span>
+        </a>
+    </div>
+</div>
