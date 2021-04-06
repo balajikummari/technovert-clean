8dd44829d9f2f0d0a550778c68c910e8e1eb3ce2
Shubham <Ks6422@hotmail.com>
2021-04-06 18:31:33 +0530

    fixed white paper typo

--- template-parts/insights-nav.php
+++ template-parts/insights-nav.php
@@ -24,7 +24,7 @@
                 <a class="nav-link" href="/events">Events</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="/white-paper">Events</a>
+                <a class="nav-link" href="/white-paper">White Paper</a>
             </li>
         </ul>
     </div>
