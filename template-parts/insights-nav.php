341995aeee71f56a823a4433feb6de6770acdf14
Shubham <Ks6422@hotmail.com>
2021-04-06 18:30:24 +0530

    added white paper link in insight nav

--- template-parts/insights-nav.php
+++ template-parts/insights-nav.php
@@ -23,6 +23,9 @@
             <li class="nav-item">
                 <a class="nav-link" href="/events">Events</a>
             </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/white-paper">Events</a>
+            </li>
         </ul>
     </div>
 </nav>
