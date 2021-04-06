88d722be67d1b3227e376a5b5ce2fb0a0e8694d4
Amol Borkar <amolborkar5@gmail.com>
2021-04-06 13:10:45 +0530

    Added more categories

--- template-parts/vertical-nav.php
+++ template-parts/vertical-nav.php
@@ -9,23 +9,35 @@
 ?>
 
 
-  <ul class="vertical-nav">
-    <li class="nav-items">
-      <a href="/blog" class="nav-link active">All Topics</a>
-    </li>
-    <li class="nav-items">
-      <a href="/category/digital-transformation/" class="nav-link">Digital Transformation</a>
-    </li>
-    <li class="nav-items">
-      <a href="/category/cloud/" class="nav-link">Cloud</a>
-    </li>
-    <li class="nav-items">
-      <a href="/category/product-engineering/" class="nav-link">Product Engineering</a>
-    </li>
-    <li class="nav-items">
-      <a href="/category/quality-engineering/" class="nav-link">Quality Engineering</a>
-    </li>
-    <li class="nav-items">
-      <a href="/category/user-experience/" class="nav-link">User Experience</a>
-    </li>
-  </ul>
+<ul class="vertical-nav">
+  <li class="nav-items">
+    <a href="/blog" class="nav-link active">All Topics</a>
+  </li>
+  <li class="nav-items">
+    <a href="/category/digital-transformation/" class="nav-link">Digital Transformation</a>
+  </li>
+  <li class="nav-items">
+    <a href="/category/cloud/" class="nav-link">Cloud</a>
+  </li>
+  <li class="nav-items">
+    <a href="/category/product-engineering/" class="nav-link">Product Engineering</a>
+  </li>
+  <li class="nav-items">
+    <a href="/category/quality-engineering/" class="nav-link">Quality Engineering</a>
+  </li>
+  <li class="nav-items">
+    <a href="/category/user-experience/" class="nav-link">User Experience</a>
+  </li>
+  <li class="nav-items">
+    <a href="/category/insurance/" class="nav-link">User Experience</a>
+  </li>
+  <li class="nav-items">
+    <a href="/category/digital-workplace/" class="nav-link">User Experience</a>
+  </li>
+  <li class="nav-items">
+    <a href="/category/data-analytics/" class="nav-link">User Experience</a>
+  </li>
+  <li class="nav-items">
+    <a href="/category/office-365/" class="nav-link">User Experience</a>
+  </li>
+</ul>
